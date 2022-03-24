7a1e014034f0db6e77054c34d09c2d3d06b1c5a3
Maja <majamaj2003@gmail.com>
2022-03-24 12:47:33 +0000

    Added intro

--- biuro-podrozy/src/components/Intro/Intro.js
+++ biuro-podrozy/src/components/Intro/Intro.js
@@ -2,7 +2,12 @@ import styles from './Intro.module.css';
 
 function Intro(){
     return(
+        <div>
         <div className={styles.intro}>
+        </div>
+        <h1 className={styles.introText}>Travel with us</h1>
+        <hr></hr>
+        <p className={styles.introPar}>Choose place and we'll create your ideal trip</p>
         </div>
     )
 }
--- biuro-podrozy/src/components/Menu/Menu.js
+++ biuro-podrozy/src/components/Menu/Menu.js
@@ -4,10 +4,10 @@ function Menu() {
     return (
         <div className={styles.containerMenu}>
             <ul className={styles.list}>
-            <li className={styles.item}>Home</li>
-            <li className={styles.item}>Gallery</li>
-            <li className={styles.item}>Abous us</li>
-            <li className={styles.item}>Contact</li>
+            <li className={styles.item}><a href="#">Home</a></li>
+            <li className={styles.item}><a href="#">Gallery</a></li>
+            <li className={styles.item}><a href="#">Abous us</a></li>
+            <li className={styles.item}><a href="#">Contact</a></li>
             </ul>
         </div>
     )
